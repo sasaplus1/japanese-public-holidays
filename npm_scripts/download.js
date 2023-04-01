ce76333b265288f94600d2858ac3a0cf17331464
sasaplus1 <sasaplus1@gmail.com>
2023-04-01 09:13:28 +0900

    download directly

--- npm_scripts/download.js
+++ npm_scripts/download.js
@@ -2,23 +2,19 @@ const download = require('download');
 const iconvLite = require('iconv-lite');
 
 const dataUrl = new URL(
-  '/data/api/action/resource_show?id=d9ad35a5-6c9c-4127-bdbe-aa138fdffe42',
-  'https://www.data.go.jp'
+  '/chosei/shukujitsu/syukujitsu.csv',
+  'https://www8.cao.go.jp'
 );
 
-download(dataUrl.toString())
-  .then(function (buffer) {
-    const json = buffer.toString('utf8');
-    const data = JSON.parse(json);
-
-    const { url } = (data || {}).result;
-
-    download(url)
+async function main() {
+  try {
+    await download(dataUrl)
       .pipe(iconvLite.decodeStream('cp932'))
       .pipe(iconvLite.encodeStream('utf8'))
       .pipe(process.stdout);
-  })
-  .catch(function (error) {
+  } catch (error) {
     process.exitCode = 1;
-    console.error(error);
-  });
+    console.log(error);
+  }
+}
+main();
