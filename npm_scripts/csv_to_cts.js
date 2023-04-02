ca680bd249a24f0eab28edcfcc1428b14b7af693
sasaplus1 <sasaplus1@gmail.com>
2023-04-02 21:35:38 +0900

    split converter script

---
+++ npm_scripts/csv_to_cts.js
@@ -0,0 +1,38 @@
+const { parse: csvParse } = require('csv-parse');
+const through2 = require('through2');
+
+const { columns, convertToIso8601, filterOutPasts } = require('./utils');
+
+const holidays = [];
+
+const isFutureOnly = process.argv[2] === '--future-only';
+
+const csvParseOptions = {
+  from_line: 2,
+  columns
+};
+
+if (isFutureOnly) {
+  csvParseOptions.on_record = filterOutPasts(new Date());
+}
+
+process.stdin
+  .pipe(csvParse(csvParseOptions))
+  .pipe(
+    through2({
+      objectMode: true
+    })
+  )
+  .on('data', function (data) {
+    const { date, name } = data;
+
+    holidays.push({
+      date: convertToIso8601(date),
+      name
+    });
+  })
+  .on('end', function () {
+    process.stdout.write(
+      `module.exports = { holidays: ${JSON.stringify(holidays, null, 2)} };`
+    );
+  });
